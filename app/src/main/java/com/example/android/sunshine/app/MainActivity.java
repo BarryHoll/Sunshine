6cc5573e63a0ee1e708eabb7967171bbc50ff334
BarryHoll <bchollgithub@gmail.com>
2015-06-27 20:35:57 +0100

    Implemented simple list based UI that's populated using an adapter.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -57,6 +63,26 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            String [] fakeForecastArray = new String [] {
+                    "Today - Sunny - 88 / 63",
+                    "Tomorrow - Foggy - 70 / 46", "Weds - Cloudy - 72 / 63",
+                    "Thurs - Rainy - 64 / 51", "Fri - Foggy - 70 / 46",
+                    "Sat - Sunny - 76 / 68"
+            };
+
+            List<String> weeksForecast = new ArrayList<>(
+                    Arrays.asList(fakeForecastArray));
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forcast_textview,
+                    weeksForecast);
+
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forcast);
+            listView.setAdapter(adapter);
+
             return rootView;
         }
     }
